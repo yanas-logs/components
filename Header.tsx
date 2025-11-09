115ed2e6f6761636687c933c3c609921d8dbe0c8
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-09 15:58:05 +0000

    Refactor Header component for improved formatting

--- Header.tsx
+++ Header.tsx
@@ -1,18 +1,16 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-<div className="container mx-auto">
-<Link href="/">
-<h1 className="text-4xl font-semibold>
-Logo<span className="text-accent">.</span>
-</h1>
-</Link>
-</div>
+      <div className="container mx-auto">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Logo<span className="text-accent">.</span>
+          </h1>
+        </Link>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header;
